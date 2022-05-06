d6b90ba6e655c0daed848ce64eed054636cf13b6
OscarReyes-dev <oscar.rdev@gmail.com>
2022-05-06 16:34:19 -0700

    Added new changes

---
+++ assets/javascript/collection-page.js
@@ -0,0 +1,19 @@
+var filterBoxToggler = document.querySelector(".toggle-filters");
+var filterBox = document.querySelector(".filter-box");
+
+var x = document.getElementById("filterbox-Toggler");
+
+filterBoxToggler.addEventListener("click", function() {
+    filterBox.classList.toggle("hide-Filter--Box");
+
+
+
+
+    if (x.innerHTML === "Hide Filters") {
+      x.innerHTML = "Show Filters";
+    } else {
+      x.innerHTML = "Hide Filters";
+    }
+
+  });
+
